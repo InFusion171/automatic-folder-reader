f8e859a48bcf8220174603a1812987e1d6e3d7a4
Marius Degenhardt <marius.degenhardt@stud.uni-goettingen.de>
2026-01-10 18:39:47 +0100

    refactored and used correct query params

--- js/background.js
+++ js/background.js
@@ -1,23 +1,21 @@
 async function onFolderInfoChangedListener(folder, folderInfo) {
-	if (!commonFolders) 
-		return;
-	
-	if (folderInfo.unreadMessageCount > 0) {
-		messenger.messages.query({"folder": folder, "unread": true}).then(
-			(messageList) => {
-				for (let message of messageList.messages) {
-					messenger.messages.update(message.id, {"read": true});
-				}
-			},
-			(error) => {
-				console.error("background: " + folder + "-folder: " + error);
+	if (folderInfo.unreadMessageCount == 0)
+		return
+
+	messenger.messages.query({"folderId": folder.id, "read": false}).then(
+		(m) => {
+			for (let message of m.messages) {
+				messenger.messages.update(message.id, {"read": true});
 			}
-		);
-	}
+		},
+		(error) => {
+			console.error("[Auto read Addon]: " + folder + "-folder: " + error);
+		}
+	);
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-	console.info("[Addon]: autoread_folder loaded");
+	console.info("[Auto read Addon]: autoread_folder loaded");
 
 	messenger.folders.onFolderInfoChanged.addListener(onFolderInfoChangedListener);
 });
